883f7a81e65ac45ead80df3b8926353514db8da5
henrisusanto <henri@member.id>
2021-02-12 21:44:16 +0700

    prevent graphql rate limited

--- promote/ig.js
+++ promote/ig.js
@@ -5,7 +5,7 @@ const nightmare = Nightmare({
         images: false
     }
 })
-var commented_accounts = ['lpkntraining', 'ulfayul88', 'patralearningcenter', 'pelatihan.lpkn', 'mmustika58', 'gitaslvi1', 'klikmro', 'pengadaanku', 'cv.diannafiriadikarya', 'datalelang.id', 'terraland_officechair', 'wensora867', 'bpbj_kutaibarat', 'samijayamitrautama', 'koranbaliexpress', 'seminar.land', 'tenderonlineid', 'lapakmesinindo', 'iwan.tms', 'unipro_topi', 'caspercomputerboyolali', 'prawira_jaya_kahutama', 'ratnafjr96', 'mamika_home', 'pmkonsulindo', 'emeraldapparel.id']
+var commented_accounts = ['lpkntraining', 'ulfayul88', 'patralearningcenter', 'pelatihan.lpkn', 'mmustika58', 'gitaslvi1', 'klikmro', 'pengadaanku', 'cv.diannafiriadikarya', 'datalelang.id', 'terraland_officechair', 'wensora867', 'bpbj_kutaibarat', 'samijayamitrautama', 'koranbaliexpress', 'seminar.land', 'tenderonlineid', 'lapakmesinindo', 'iwan.tms', 'unipro_topi', 'caspercomputerboyolali', 'prawira_jaya_kahutama', 'ratnafjr96', 'mamika_home', 'pmkonsulindo', 'emeraldapparel.id', 'kppnmajene', 'rackbesijabar']
 const comment = `permisi kak, mohon izin berbagi info pengadaan terbaru seluruh Indonesia gratis, cek linknya di bio kita. terima kasih`
 
 nightmare
@@ -15,7 +15,7 @@ nightmare
     .type('[name="password"]', 'x123123x')
     .click('[type="submit"]')
     .wait('[placeholder="Search"]')
-    .type('[placeholder="Search"]', '#pengadaan')
+    .type('[placeholder="Search"]', '#pengadaanbarangjasa')
     .wait('[href^="/explore/tags/"]')
     .click('[href^="/explore/tags/"]')
     .wait('h1')
@@ -32,7 +32,11 @@ function walkThroughModal(prevURL) {
         .wait(prevURL => {
             return window.location.href !== prevURL
         }, prevURL)
+        .evaluate(() => {// DEBUG
+            console.log(new Date().toString().split(' ')[4], window.location.href)
+        })
         .wait('body > div > div > div > article > div > div > a > time')
+        .wait(1000) // PREVENT IG GRAPHQL RATE LIMITED
         .evaluate(() => {
             let time = $('body > div > div > div > article > div > div > a > time').html()
             let username = $('body > div > div > div > article > header > div > div > div > span > a').text()
@@ -40,7 +44,7 @@ function walkThroughModal(prevURL) {
         })
         .then(data => {
             let { time, username } = data
-            if ((time.indexOf('hours ago') > -1 || time.indexOf('days ago') > -1) && commented_accounts.indexOf(username) < 0) {
+            if ((time.indexOf('hours ago') > -1 || time.indexOf('day ago') > -1 || time.indexOf('days ago') > -1) && commented_accounts.indexOf(username) < 0) {
                 nightmare
                     .wait('body > div > div > div > article > div > section > div > form > textarea')
                     .type('body > div > div > div > article > div > section > div > form > textarea', comment)
@@ -61,5 +65,12 @@ function walkThroughModal(prevURL) {
                     .click('.coreSpriteRightPaginationArrow')
                     .then(walkThroughModal)
             }
+        })
+        .catch(e => {
+            nightmare
+                .url()
+                .then(url => {
+                    walkThroughModal(url)
+                })
         })
 }
